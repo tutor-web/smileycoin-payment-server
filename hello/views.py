118026b923292d3fa1192146a1b9d7af14c7e8c4
andreabjork <andreabjork43@gmail.com>
2016-06-10 10:27:17 +0000

    A backbone for our smileycoin service

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,20 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests
+
 # Create your views here.
 def index(request):
+    # ORIGINAL
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
+    #r = requests.get('http://httpbin.org/status/418')
+    #print r.text
+    #return HttpResponse('<pre>' + r.text + '</pre>')
 
+
+def generateAddress(request):
+    return HttpResponse('{address: here is where our address will go}')
 
 def db(request):
 
