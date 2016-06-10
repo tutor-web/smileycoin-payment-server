68c997923872c8789ad7c57fac55ee6f7d989cd9
andreabjork <andreabjork43@gmail.com>
2016-06-10 11:12:53 +0000

    moving test into smileycoin.py

--- smileycoin.py
+++ smileycoin.py
@@ -3,3 +3,5 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.call('./smileycoind --server &', shell=True)
+    print 'Weve called the smileycoind daemon to start'
+    print subprocess.check_output('./smileycoind getnewaddress', shell=True)
