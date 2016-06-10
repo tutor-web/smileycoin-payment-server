5ca1abde42c8fa4f49e76a48a563d8f364e52225
andreabjork <andreabjork43@gmail.com>
2016-06-10 12:43:30 +0000

    trying to sleep the thread so it doesn't crash

--- smileycoin.py
+++ smileycoin.py
@@ -1,6 +1,8 @@
 import subprocess
+import time
 
 
 if __name__ == '__main__':
     subprocess.call('./smileycoind --server &', shell=True)
+    time.sleep(10)
     print subprocess.check_output('./smileycoind getnewaddress', shell=True)
