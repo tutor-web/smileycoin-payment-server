5603dd080ad1fe7d4aa2064b387bf6e87047e721
andreabjork <andreabjork43@gmail.com>
2016-06-10 12:36:23 +0000

    more testing...

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn gettingstarted.wsgi --log-file -
-worker: bash ./smileycoind --server &
+worker: python smileycoin.py
--- smileycoin.py
+++ smileycoin.py
@@ -3,3 +3,4 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.call('./smileycoind --server &', shell=True)
+    print subprocess.check_output('./smileycoind getnewaddress', shell=True)
