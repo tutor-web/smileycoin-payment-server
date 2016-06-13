4843b37428c72a3d450670e9e0adac0f08018928
andreabjork <andreabjork43@gmail.com>
2016-06-13 15:19:28 +0000

    The uuid is too long, accept longer strings

--- hello/models.py
+++ hello/models.py
@@ -5,5 +5,5 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class Payment(models.Model):
-	userId = models.CharField(max_length=30)
-	address = models.CharField(max_length=30)
+	userId = models.CharField(max_length=60)
+	address = models.CharField(max_length=60)
