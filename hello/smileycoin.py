39861b19a0170a9e1dbd894d749c877713083ba4
andreabjork <andreabjork43@gmail.com>
2016-06-13 14:26:51 +0000

    Trying to save payments to database

---
+++ hello/smileycoin.py
@@ -0,0 +1,30 @@
+import uuid
+
+class Smileycoin():
+
+	# Starts the daemon if it isn't already up.
+	# Runs the getnewaddress command on the smileycoin daemon.
+	def getAddress():
+		# Check if smileycoind is running and start it if it isnt
+	    try:
+	        subprocess.check_output('pgrep smileycoind', shell=True)
+	    except CalledProcessError, e:
+	        subprocess.call('./smileycoind --server &', shell=True)
+	    finally:
+	        # After smileycoind has started, get 10 tries to generate an address
+	        # We might need multiple tries because if smileycoind was not running, it
+	        # might take a while to start up
+	        numTries = 10
+	        output = 0
+	        while(numTries >= 0):
+	            try:
+	                output = subprocess.check_output('./smileycoind getnewaddress', shell=True)[:-1]
+	            except CalledProcessError, e:
+	                print "Trying again after 50 ms, "+numTries+" left."
+	                numTries = numTries-1
+	                time.sleep(0.05)
+
+	        return output
+
+	def getUserID():
+		return uuid.uuid4()
--- smileycoin.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-
-
-if __name__ == '__main__':
-    subprocess.call('./smileycoind --server &', shell=True)
-    time.sleep(10)
-    print subprocess.check_output('./smileycoind getnewaddress', shell=True)
