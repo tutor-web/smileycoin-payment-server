24827974e2604337a8bb06b26b47f29e943f0364
Andrea Björk <andreabjork43@gmail.com>
2016-06-14 11:34:17 +0000

    replacing tabs by spaces2

--- hello/smileycoin.py
+++ hello/smileycoin.py
@@ -4,41 +4,41 @@ import time
 
 class Smileycoin():
 
-	# Starts the daemon if it isn't already up.
-	# Runs the getnewaddress command on the smileycoin daemon.
-	def getAddress(self):
-		# Check if smileycoind is running and start it if it isnt
-	    try:
-	    	print "pgrepping for smileycoin daemon"
-	    	print "At first, lets just see if we can actually run the notifytest: "
-			subprocess.call("./notifytest.sh", shell=True)
-	        subprocess.check_output('pgrep smileycoind', shell=True)
-	        print "seems the daemon is already running, continue ..."
-	    except subprocess.CalledProcessError, e:
-	    	print "CalledProcessError occurred, starting smileycoind ..."
-	        print "At first, lets just see if we can actually run the notifytest: "
-			subprocess.call("./notifytest.sh", shell=True)
-	        subprocess.call('./smileycoind --server -walletnotify=./notifytest.sh &', shell=True)
-	        print "Smileycoin daemon started"
-	    finally:
-	        # After smileycoind has started, get 10 tries to generate an address
-	        # We might need multiple tries because if smileycoind was not running, it
-	        # might take a while to start up
-	        print "take 10 tries to get the new address"
-	        numTries = 10
-	        output = 0
-	        while(numTries >= 0):
-	            try:
-	            	print "trying to get new address ..."
-	                output = subprocess.check_output('./smileycoind getnewaddress', shell=True)[:-1]
-	                break
-	            except subprocess.CalledProcessError, e:
-	                print "Getnewaddress failed. Trying again after 50 ms, ", numTries, " left."
-	                numTries = numTries-1
-	                time.sleep(0.05)
+    # Starts the daemon if it isn't already up.
+    # Runs the getnewaddress command on the smileycoin daemon.
+    def getAddress(self):
+        # Check if smileycoind is running and start it if it isnt
+        try:
+            print "pgrepping for smileycoin daemon"
+            print "At first, lets just see if we can actually run the notifytest: "
+            subprocess.call("./notifytest.sh", shell=True)
+            subprocess.check_output('pgrep smileycoind', shell=True)
+            print "seems the daemon is already running, continue ..."
+        except subprocess.CalledProcessError, e:
+            print "CalledProcessError occurred, starting smileycoind ..."
+            print "At first, lets just see if we can actually run the notifytest: "
+            subprocess.call("./notifytest.sh", shell=True)
+            subprocess.call('./smileycoind --server -walletnotify=./notifytest.sh &', shell=True)
+            print "Smileycoin daemon started"
+        finally:
+            # After smileycoind has started, get 10 tries to generate an address
+            # We might need multiple tries because if smileycoind was not running, it
+            # might take a while to start up
+            print "take 10 tries to get the new address"
+            numTries = 10
+            output = 0
+            while(numTries >= 0):
+                try:
+                    print "trying to get new address ..."
+                    output = subprocess.check_output('./smileycoind getnewaddress', shell=True)[:-1]
+                    break
+                except subprocess.CalledProcessError, e:
+                    print "Getnewaddress failed. Trying again after 50 ms, ", numTries, " left."
+                    numTries = numTries-1
+                    time.sleep(0.05)
 
-	        print "While loop finished, returning ..."
-	        return output
+            print "While loop finished, returning ..."
+            return output
 
-	def getUserID(self):
-		return str(uuid.uuid4())
+    def getUserID(self):
+        return str(uuid.uuid4())
