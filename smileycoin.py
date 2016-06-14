7ab22c59092b5386dd38ae65fabd9c494b9c91ab
Andrea Björk <andreabjork43@gmail.com>
2016-06-14 11:00:04 +0000

    still here for testing purposes, will be removed

--- smileycoin.py
+++ smileycoin.py
@@ -1,8 +1,9 @@
 import subprocess
 import time
 
-
 if __name__ == '__main__':
+    # This is deprecated, for now we will not run this as a worker thread,
+    # this function is called directly from views.py
     subprocess.call('./smileycoind --server &', shell=True)
     time.sleep(10)
     print subprocess.check_output('./smileycoind getnewaddress', shell=True)
