c4d5c0b1ed99ef047b51ab27691f76f5776bc8eb
andreabjork <andreabjork43@gmail.com>
2016-06-15 14:17:06 +0000

    This should work now

---
+++ receiveTX.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys
+from hello.models import PaymentRequest
+
+print 'CALLING PYTHON!!!!! BO-OOM!'
+paymentReq = PaymentRequest(userID="THIS IS JUST FOR TESTING", address=sys.argv[1])
+paymentReq.save()
