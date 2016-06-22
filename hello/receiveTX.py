b5654310d0f82d3b4e5da2e068eecd3369fe46cc
andreabjork <andreabjork43@gmail.com>
2016-06-22 13:02:51 +0000

    localhost to server

--- hello/receiveTX.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-from models import PaymentRequest
-
-print 'CALLING PYTHON!!!!! BO-OOM!'
-paymentReq = PaymentRequest(userID="THIS IS JUST FOR TESTING", address="This is also just for testing")
-paymentReq.save()
--- notifyTX.sh
+++ notifyTX.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tokenHeaders=$(curl -I http://127.0.0.1:5000/getToken)
+tokenHeaders=$(curl -I http://smileyservice.herokuapp.com/getToken)
 token=$(grep -oP '(?<=csrftoken=).*?(?=;)' <<< "$tokenHeaders")
 echo "Got csrf token: $token"
 
-curl -d "$1" --header "X-CSRFToken:$token" --cookie "csrftoken=$token" http://127.0.0.1:5000/postTX
+curl -d "$1" --header "X-CSRFToken:$token" --cookie "csrftoken=$token" http://smileyservice.herokuapp.com/postTX
 
