c778cbdcaa07ca1714fa3f445f7f537c0cc46fdc
Andrea Björk <andreabjork43@gmail.com>
2016-08-10 15:48:20 +0000

    Fixed border color on verify box

--- hello/static/uiControl.js
+++ hello/static/uiControl.js
@@ -29,10 +29,12 @@ setMessage = function(message) {
 changeVerifyBox = function(verified) {
 	if(!verified) {
 		$("#verifyBox").css("background-color", "#f2511d");
+		$("#verifyBox").css("border", "2px solid #ec8c47");
 		document.getElementById("verifyText").innerHTML = "Greiðsla óstaðfest";
 		verified = false;
 	} else {
 		$("#verifyBox").css("background-color", "#47BD4E");
+		$("#verifyBox").css("border", "2px solid #91ec47");
 		document.getElementById("verifyText").innerHTML = "Greiðsla staðfest";
 		verified = true;
 	}
